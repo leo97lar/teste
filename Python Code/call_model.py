e48a5eb42764e11bed775ae5464b250b87b7b561
Leonardo Rocha <leo97lar@gmail.com>
2020-11-22 12:26:39 -0300

    Allow header on csv and start teste.html

--- Python Code/call_model.py
+++ Python Code/call_model.py
@@ -12,115 +12,146 @@ from ctypes import CDLL, c_void_p, c_int, c_double, c_uint8, POINTER, byref
 from time import time
 from config import dll, csv_folder, k0, k1, k2, k3, numIQ, numIC, taxC, taxE, taxEQ, py_ProbXEst, genToWidth, generations, keeppriority
 
-inicio = time()
+headers = {
+    'CP': False,
+    'RO': False,
+    'CPO': False,
+    'CPrO': False,
+    'CR': False,
+    'Data': False,
+    'Dia': False,
+    'DispMExD': False,
+    'MA': False,
+    'MAn': False,
+    'ME': False,
+    'S': False,
+}
 
-model = CDLL(dll)
-
-model.emxCreate_int32_T.restype = c_void_p
-model.emxCreate_real_T.restype = c_void_p
-model.int_array_to_emxArray.argtypes = [c_void_p, c_void_p, c_int]
-model.real_array_to_emxArray.argtypes = [c_void_p, c_void_p, c_int]
-model.emxDestroyArray_int32_T.argtypes = [c_void_p]
-model.emxDestroyArray_real_T.argtypes = [c_void_p]
-model.main_UCI_func.restype = POINTER(c_int)
-model.main_UCI_func.argtypes = [
-    c_void_p, c_void_p, c_void_p, c_void_p, c_void_p, c_void_p, c_void_p, c_void_p, c_void_p, c_void_p, c_void_p, c_void_p,
-    c_int, c_int, c_int, c_int, c_int, c_int, c_int,
-    c_int, c_int, c_int, c_int, c_double, c_double, c_double, c_double,
-    c_int, c_int, c_double, c_double, c_double, c_void_p, c_int,
-    c_int, c_uint8,
-]
-
-#%% Load CSV
-
-
-def read_c_csv(filepath, header):
-    df = pd.read_csv(filepath, header=None, skiprows=int(header))
-    flat = df.values.flatten()
-    arr = (c_int * df.size)(*flat)
-
-    c_mat = model.emxCreate_int32_T(df.shape[0], df.shape[1])
-
-    model.int_array_to_emxArray(arr, c_mat, df.size)
-
-    return c_mat, df.shape
+def _get_shape(filename, shape=None):
+    df = pd.read_csv(csv_folder + filename + ".csv", header=None, skiprows=int(headers[filename]))
+    if shape is not None:
+        return df.shape[shape]
+    return df.shape
 
 
-#   Initialize the application.
-#   You do not need to do this more than one time.
-model.model_initialize()
+def get_hospital_values():
+    return {
+        'NumSalOp': _get_shape('S', 0),     # Numero de Salones de operaciones
 
-files = [
-    ('CP', False), ('RO', False), ('CPO', False), ('CPrO', False),
-    ('CR', False), ('Data', False), ('Dia', False), ('DispMExD', False),
-    ('MA', False), ('MAn', False), ('ME', False), ('S', False)
-]
-csvs = {}
+        'NumCPO':   _get_shape('CPO', 0),   # Numero de camas Post - Operatorias
+        'NumCPrO':  _get_shape('CPrO', 0),  # Numero de camas Pre - Operatorias
+        'NumCR':    _get_shape('CR', 0),    # Numero de camas Recuperacion
 
-for filename, header in files:
-    data, shape = read_c_csv(csv_folder + filename + ".csv", header=header)
-    csvs[filename] = {'data': data, 'shape': shape}
+        'NumMedEsp':_get_shape('ME', 0),    # Numero de Medicos Especialistas
+        'NumAsist': _get_shape('MA', 0),    # Numero de Medico Asistentes
+        'NumAnest': _get_shape('MAn', 0),   # Numero de Anestesistas
+    }
 
-#%% Inicializar Variáveis
+def run_model():
+    inicio = time()
 
-#   Variables de Interes
+    model = CDLL(dll)
 
-TipoOp = csvs['RO']['shape'][0]  # Tipos de operaciones
-NumEsp = csvs['ME']['shape'][1]  # Numero de Especialidades
-NumTOp = csvs['CP']['shape'][0]  # Numero Total de Operaciones
+    model.emxCreate_int32_T.restype = c_void_p
+    model.emxCreate_real_T.restype = c_void_p
+    model.int_array_to_emxArray.argtypes = [c_void_p, c_void_p, c_int]
+    model.real_array_to_emxArray.argtypes = [c_void_p, c_void_p, c_int]
+    model.emxDestroyArray_int32_T.argtypes = [c_void_p]
+    model.emxDestroyArray_real_T.argtypes = [c_void_p]
+    model.main_UCI_func.restype = POINTER(c_int)
+    model.main_UCI_func.argtypes = [
+        c_void_p, c_void_p, c_void_p, c_void_p, c_void_p, c_void_p, c_void_p, c_void_p, c_void_p, c_void_p, c_void_p, c_void_p,
+        c_int, c_int, c_int, c_int, c_int, c_int, c_int,
+        c_int, c_int, c_int, c_int, c_double, c_double, c_double, c_double,
+        c_int, c_int, c_double, c_double, c_double, c_void_p, c_int,
+        c_int, c_uint8,
+    ]
 
-#   Recursos materiales
-NumSalOp = csvs['S']['shape'][0]  # Numero de Salones de operaciones
-NumCPO = csvs['CPO']['shape'][0]  # Numero de camas Post - Operatorias
-NumCPrO = csvs['CPrO']['shape'][0]  # Numero de camas Pre - Operatorias
-NumCR = csvs['CR']['shape'][0]  # Numero de camas Recuperacion
-
-NumMedEsp = csvs['ME']['shape'][0]  # Numero de Medicos Especialistas
-NumEspxE = int(NumMedEsp/NumEsp)  # Numero de Especialistas por Especialidad
-NumAsist = csvs['MA']['shape'][0]  # Numero de Medico Asistentes
-NumAnest = csvs['MAn']['shape'][0]  # Numero de Anestesistas
-
-
-ProbXEst = model.emxCreate_real_T(5, 1)
-
-c_ProbXEst = (c_double * len(py_ProbXEst))(*py_ProbXEst)
-model.real_array_to_emxArray(c_ProbXEst, ProbXEst, len(py_ProbXEst))
+    #%% Load CSV
 
 
-#%% Rodar Modelo
+    #   Initialize the application.
+    #   You do not need to do this more than one time.
+    model.model_initialize()
 
-# 	/* Call the entry-point 'main_UCI_func'. */
-c_queue = model.main_UCI_func(csvs['CP']['data'], csvs['RO']['data'], csvs['CPO']['data'], csvs['CPrO']['data'], csvs['CR']['data'], csvs['Data']['data'], csvs['Dia']['data'], csvs['DispMExD']['data'], csvs['MA']['data'], csvs['MAn']['data'], csvs['ME']['data'], csvs['S']['data'],
-                              TipoOp, NumEsp, NumTOp, NumSalOp, NumCPO, NumCPrO, NumCR,
-                              NumMedEsp, NumEspxE, NumAsist, NumAnest, k0, k1, k2, k3,
-                              numIQ, numIC, taxC, taxE, taxEQ, ProbXEst, genToWidth,
-                              generations, keeppriority)
+    csvs = {}
 
-#%% Testar resultado
-print('rodou: ' + str(c_queue))
-a = time()
-queue = [c_queue[i] for i in range(NumTOp)]
+    def read_c_csv(filepath, header):
+        df = pd.read_csv(filepath, header=None, skiprows=int(header))
+        flat = df.values.flatten()
+        arr = (c_int * df.size)(*flat)
 
-print('conversão: ' + str(time()-a) + "\n" + str(queue))
+        c_mat = model.emxCreate_int32_T(df.shape[0], df.shape[1])
 
-#%%Terminate model
+        model.int_array_to_emxArray(arr, c_mat, df.size)
 
-model.emxDestroyArray_int32_T(csvs['S'])
-model.emxDestroyArray_int32_T(csvs['ME'])
-model.emxDestroyArray_int32_T(csvs['MAn'])
-model.emxDestroyArray_int32_T(csvs['MA'])
-model.emxDestroyArray_int32_T(csvs['DispMExD'])
-model.emxDestroyArray_int32_T(csvs['Dia'])
-model.emxDestroyArray_int32_T(csvs['Data'])
-model.emxDestroyArray_int32_T(csvs['CR'])
-model.emxDestroyArray_int32_T(csvs['CPrO'])
-model.emxDestroyArray_int32_T(csvs['CPO'])
-model.emxDestroyArray_int32_T(csvs['RO'])
-model.emxDestroyArray_int32_T(csvs['CP'])
-model.emxDestroyArray_int32_T(c_queue)
+        return c_mat, df.shape
 
-model.emxDestroyArray_real_T(ProbXEst)
+    for filename, header in headers.items():
+        data, shape = read_c_csv(csv_folder + filename + ".csv", header=header)
+        csvs[filename] = {'data': data, 'shape': shape}
 
-model.model_terminate()
+    #%% Inicializar Variáveis
 
-print('Tempo total: ' + str(time() - inicio) + ' segundos')
+    #   Variables de Interes
+
+    TipoOp = csvs['RO']['shape'][0]  # Tipos de operaciones
+    NumEsp = csvs['ME']['shape'][1]  # Numero de Especialidades
+    NumTOp = csvs['CP']['shape'][0]  # Numero Total de Operaciones
+
+    #   Recursos materiales
+    NumSalOp = csvs['S']['shape'][0]  # Numero de Salones de operaciones
+    NumCPO = csvs['CPO']['shape'][0]  # Numero de camas Post - Operatorias
+    NumCPrO = csvs['CPrO']['shape'][0]  # Numero de camas Pre - Operatorias
+    NumCR = csvs['CR']['shape'][0]  # Numero de camas Recuperacion
+
+    NumMedEsp = csvs['ME']['shape'][0]  # Numero de Medicos Especialistas
+    NumEspxE = int(NumMedEsp/NumEsp)  # Numero de Especialistas por Especialidad
+    NumAsist = csvs['MA']['shape'][0]  # Numero de Medico Asistentes
+    NumAnest = csvs['MAn']['shape'][0]  # Numero de Anestesistas
+
+    ProbXEst = model.emxCreate_real_T(5, 1)
+
+    c_ProbXEst = (c_double * len(py_ProbXEst))(*py_ProbXEst)
+    model.real_array_to_emxArray(c_ProbXEst, ProbXEst, len(py_ProbXEst))
+
+    #%% Rodar Modelo
+
+    # 	/* Call the entry-point 'main_UCI_func'. */
+    c_queue = model.main_UCI_func(csvs['CP']['data'], csvs['RO']['data'], csvs['CPO']['data'], csvs['CPrO']['data'], csvs['CR']['data'], csvs['Data']['data'], csvs['Dia']['data'], csvs['DispMExD']['data'], csvs['MA']['data'], csvs['MAn']['data'], csvs['ME']['data'], csvs['S']['data'],
+                                  TipoOp, NumEsp, NumTOp, NumSalOp, NumCPO, NumCPrO, NumCR,
+                                  NumMedEsp, NumEspxE, NumAsist, NumAnest, k0, k1, k2, k3,
+                                  numIQ, numIC, taxC, taxE, taxEQ, ProbXEst, genToWidth,
+                                  generations, keeppriority)
+
+    #%% Testar resultado
+    print('rodou: ' + str(c_queue))
+    a = time()
+    queue = [c_queue[i] for i in range(NumTOp)]
+
+    print('conversão: ' + str(time()-a) + "\n" + str(queue))
+
+    #%%Terminate model
+
+    model.emxDestroyArray_int32_T(csvs['S'])
+    model.emxDestroyArray_int32_T(csvs['ME'])
+    model.emxDestroyArray_int32_T(csvs['MAn'])
+    model.emxDestroyArray_int32_T(csvs['MA'])
+    model.emxDestroyArray_int32_T(csvs['DispMExD'])
+    model.emxDestroyArray_int32_T(csvs['Dia'])
+    model.emxDestroyArray_int32_T(csvs['Data'])
+    model.emxDestroyArray_int32_T(csvs['CR'])
+    model.emxDestroyArray_int32_T(csvs['CPrO'])
+    model.emxDestroyArray_int32_T(csvs['CPO'])
+    model.emxDestroyArray_int32_T(csvs['RO'])
+    model.emxDestroyArray_int32_T(csvs['CP'])
+    model.emxDestroyArray_int32_T(c_queue)
+
+    model.emxDestroyArray_real_T(ProbXEst)
+
+    model.model_terminate()
+
+    total_time = time() - inicio
+
+    print('Tempo total: ' + str(total_time) + ' segundos')
+    return queue, total_time
--- Python Code/flask_file.py
+++ Python Code/flask_file.py
@@ -1,50 +1,46 @@
 from flask import Flask, render_template, redirect
 from random import shuffle
+from call_model import run_model, get_hospital_values
+from threading import Thread
+from collections import deque
 
 app = Flask(__name__)
+
+running_thread = None
+q = deque( maxlen=3 )
+
+
+
+def model_wrapper():
+    global running_thread
+    if not (running_thread and running_thread.is_alive()):
+        running_thread = Thread(target=run_model)
+        running_thread.start()
 
 @app.route("/")
 def render_index():
     html_file = "index.html"
-    # html_path = "./templates/"
-    # with open(html_path + html_file, 'r') as f:
-    #     html_str = f.read()
-    # return html_str
     return render_template(html_file)
+
 
 @app.route("/fila")
 def render_fila():
     html_file = "fila.html"
     fila = list(range(100))
     shuffle(fila)
-    fila_html = '<p>'
-    fila_html += '</p>\n\t\t\t\t<p>'.join(map(str,fila))
-    fila_html += '</p>'
-    #<p>{{teste}}</p>
-    # html_path = "./templates/"
-    # with open(html_path + html_file, 'r') as f:
-    #     html_str = f.read()
-    # return html_str
-    return render_template(html_file, fila=fila)
+    return render_template(html_file, fila=fila, run_model=model_wrapper, running_thread=running_thread)
+
 
 @app.route("/hospital")
 def render_hospital():
     html_file = "hospital.html"
-    # html_path = "./templates/"
-    # with open(html_path + html_file, 'r') as f:
-    #     html_str = f.read()
-    # return html_str
     return render_template(html_file)
 
 
 @app.route("/teste")
 def render_teste():
     html_file = "teste.html"
-    # html_path = "./templates/"
-    # with open(html_path + html_file, 'r') as f:
-    #     html_str = f.read()
-    # return html_str
-    return render_template(html_file)
+    return render_template(html_file, run_model=model_wrapper, hosp_values=get_hospital_values())
 
 # @app.route("/numero/<int:x>")
 # def mostrar_numero(x):
@@ -52,9 +48,5 @@ def render_teste():
 
 # return redirect("/outrapagina")
 
-# return render_template("templates\index.html")
-
-# with open(html, 'r') as f:
-#     print(f.read())
 
 app.run(port=5000, debug=True)
